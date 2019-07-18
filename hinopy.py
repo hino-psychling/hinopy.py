1265059db9f8a5bdb2ac33e4307fbc0b235b91d4
hino-psychling <49905938+hino-psychling@users.noreply.github.com>
2019-07-18 19:01:23 +0900

    big upgrade with get all!
    一気に全ての文字情報をゲットできるGet_all関数が完成しました。
    問題点というのは、nttfreqの辞書はまだjson化されていません。
    そして、opconのなかに匿名関数が入ってます。

--- hinopy.py
+++ hinopy.py
@@ -5,9 +5,11 @@
 '''
 
 import sys
+import jaconv
+import json
 print('日野ゼミ常用コマンド集合でございます！')
 # グローバル変数変数について、グッグル先生に聞いてください
-dict_nttfreq_created = False
+dict_nttfreq_created = dict_jonbs_created = dict_jmnbs_created = dict_homoph_created = dict_nttfam_created = dict_nttpfam_created = False
 re_imported = False
 
 
@@ -91,27 +93,6 @@ def make_unique(alist):
     return aresult
 
 
-# テスト中直接頻度操作などの常用ものをパッケージの中に内包する
-# 辞書の導入は時間がかかります。その故にhinopy導入する時点では、辞書の導入はなし。
-# dict_nttfreq_createdという変数で辞書は作成しているかどうかを判別。
-def get_nttfreq(aword, changeint=False):
-    global dict_nttfreq_created
-    if dict_nttfreq_created is False:
-        print('dict_nttfreq、作成中、少々お待ち下さい！')
-        dict_path = [
-            x for x in sys.path if x.split('\\')[-1][:9] == 'hinopy.py'
-        ][0] + '\\dicts\\nttfreq.dict'
-        f = open(dict_path, 'r', encoding='utf-8')
-        global dict_nttfreq
-        dict_nttfreq = eval(f.read())
-        f.close()
-        dict_nttfreq_created = True
-    aresult = dict_nttfreq.get(aword, '0')
-    if changeint is True:
-        aresult = int(aresult)
-    return aresult
-
-
 # 入力した文字列はひらかなのみであるかどうかをチェックする関数
 def is_pure_hiragana(astring):
     global re_imported
@@ -149,3 +130,285 @@ def is_pure_kanji(astring):
     if ''.join(re.findall('[一-龥]+', astring)) != astring:
         aresult = False
     return aresult
+
+
+def get_length(word_phonetic):
+    if ' ' in word_phonetic or '　' in word_phonetic:
+        res = str(len(word_phonetic.split()[0]))
+    else:
+        res = str(len(word_phonetic))
+    return res
+
+
+def get_mora(word_phonetic, simple_output=True):
+    if ' ' in word_phonetic or '　' in word_phonetic:
+        normalized_input = word_phonetic.split()[1]
+    else:
+        normalized_input = word_phonetic
+    res = list(normalized_input)
+    for hirakana in res:
+        if hirakana in 'ぁぃぅぇぉゃゅょゎ':
+            position = res.index(hirakana)
+            res.remove(hirakana)
+            res[position - 1] = res[position - 1] + hirakana
+    if simple_output is True:
+        return str(len(res))
+    else:
+        return res
+
+
+def get_nttfreq(word_phonetic, changeint=False):
+    # 語彙を扱う場合、よく表記と発音一緒に利用するから
+    # もし、表記と発音両方どもある場合、表記だけ取り出す
+    if ' ' in word_phonetic or '　' in word_phonetic:
+        normalized_input = word_phonetic.split()[0]
+    else:
+        normalized_input = word_phonetic
+    global dict_nttfreq_created
+    if dict_nttfreq_created is False:
+        print('dict_nttfreq、作成中、少々お待ち下さい！')
+        dict_path = [
+            x for x in sys.path if x.split('\\')[-1][:9] == 'hinopy.py'
+        ][0] + '\\dicts\\nttfreq.dict'
+        f = open(dict_path, 'r', encoding='utf-8')
+        global dict_nttfreq
+        dict_nttfreq = eval(f.read())
+        f.close()
+        dict_nttfreq_created = True
+    res = dict_nttfreq.get(normalized_input, '0')
+    if changeint is True:
+        res = int(res)
+    return res
+
+
+def get_fam(word_phonetic, changefloat=False):
+    # 語彙を扱う場合、よく表記と発音一緒に利用するから
+    # もし、表記と発音両方どもある場合、表記だけ取り出す
+    if ' ' in word_phonetic or '　' in word_phonetic:
+        normalized_input = word_phonetic.split()[0]
+    else:
+        normalized_input = word_phonetic
+    global dict_nttfam_created
+    if dict_nttfam_created is False:
+        print('dict_nttfam、作成中、少々お待ち下さい！')
+        dict_path = [
+            x for x in sys.path if x.split('\\')[-1][:9] == 'hinopy.py'
+        ][0] + '\\dicts\\ntt_fam.json'
+        f = open(dict_path, 'r', encoding='utf-8')
+        global d_fam
+        d_fam = json.load(f)
+        f.close()
+        dict_nttfam_created = True
+    res = d_fam.get(normalized_input, '0')
+    if changefloat is True:
+        res = float(res)
+    return res
+
+
+def get_pfam(word_phonetic, changefloat=False):
+    # 音声単語親密度だが、表記自体も重要だから、引数として、表記と音韻情報両者が必要です
+    # 音声単語親密度辞書のなか、音韻情報についての記録はカタカナなので、入力を標準化する必要がある
+    normalized_input = ' '.join(
+        [word_phonetic.split()[0],
+         jaconv.hira2kata(word_phonetic.split()[1])])
+    # 所要辞書の読み込み
+    global dict_nttpfam_created
+    if dict_nttpfam_created is False:
+        print('dict_nttpfam、作成中、少々お待ち下さい！')
+        dict_path = [
+            x for x in sys.path if x.split('\\')[-1][:9] == 'hinopy.py'
+        ][0] + '\\dicts\\ntt_pfam.json'
+        f = open(dict_path, 'r', encoding='utf-8')
+        global d_pfam
+        d_pfam = json.load(f)
+        f.close()
+        dict_nttpfam_created = True
+    res = d_pfam.get(normalized_input, '0')
+    if changefloat is True:
+        res = float(res)
+    return res
+
+
+def get_jonbs(word_phonetic, changeint=False, simple_output=True):
+    # 所要辞書の読み込み
+    global dict_jonbs_created
+    if dict_jonbs_created is False:
+        print('dict_jonbs、作成中、少々お待ち下さい！')
+        dict_path = [
+            x for x in sys.path if x.split('\\')[-1][:9] == 'hinopy.py'
+        ][0] + '\\dicts\\jonbs.json'
+        f = open(dict_path, 'r', encoding='utf-8')
+        global d_jonbs
+        d_jonbs = json.load(f)
+        f.close()
+        dict_jonbs_created = True
+    # 入力の標準化 表記だけ取り出す
+    if ' ' in word_phonetic or '　' in word_phonetic:
+        normalized_input = word_phonetic.split()[0]
+    else:
+        normalized_input = word_phonetic
+    # '大学' → ['○大', '大○']
+    l_input = []
+    for i in range(len(normalized_input)):
+        tmp_l = list(normalized_input)
+        del tmp_l[i]
+        tmp_l.insert(i, '○')
+        l_input.append(''.join(tmp_l))
+    # d_jonbsを使って、jonbsを獲得
+    l_all_jonbs = []
+    for s in l_input:
+        for line in d_jonbs.get(s, []):
+            # target語彙の除去
+            if normalized_input not in line:
+                l_all_jonbs.append(line)
+    # 結果出力部分
+    if simple_output is not True:
+        return l_all_jonbs
+    elif changeint is False:
+        return str(len(l_all_jonbs))
+    else:
+        return len(l_all_jonbs)
+
+
+def get_jmnbs(word_phonetic, changeint=False, simple_output=True):
+    # 所要辞書の読み込み
+    global dict_jmnbs_created
+    if dict_jmnbs_created is False:
+        print('dict_jmnbs、作成中、少々お待ち下さい！')
+        dict_path = [
+            x for x in sys.path if x.split('\\')[-1][:9] == 'hinopy.py'
+        ][0] + '\\dicts\\jmnbs.json'
+        f = open(dict_path, 'r', encoding='utf-8')
+        global d_jmnbs
+        d_jmnbs = json.load(f)
+        f.close()
+        dict_jmnbs_created = True
+    # 入力の標準化 音韻情報だけ取り出す
+    if ' ' in word_phonetic or '　' in word_phonetic:
+        normalized_input = word_phonetic.split()[1]
+    else:
+        normalized_input = word_phonetic
+    # 'だいがく' → ['○いがく', 'だ○がく', 'だい○く', 'だいが○']
+    l_input = []
+    for i in range(len(normalized_input)):
+        tmp_l = list(normalized_input)
+        del tmp_l[i]
+        tmp_l.insert(i, '○')
+        l_input.append(''.join(tmp_l))
+    # d_jmnbsを使って、jmnbsを獲得
+    l_all_jmnbs = []
+    for s in l_input:
+        for line in d_jmnbs.get(s, []):
+            # targetを除去
+            if normalized_input not in line:
+                l_all_jmnbs.append(line)
+    # 結果出力部分
+    if simple_output is not True:
+        return l_all_jmnbs
+    elif changeint is False:
+        return str(len(l_all_jmnbs))
+    else:
+        return len(l_all_jmnbs)
+
+
+def get_homoph(word_phonetic, changeint=False, simple_output=True):
+    # 所要辞書の読み込み
+    global dict_homoph_created
+    if dict_homoph_created is False:
+        print('dict_homoph、作成中、少々お待ち下さい！')
+        dict_path = [
+            x for x in sys.path if x.split('\\')[-1][:9] == 'hinopy.py'
+        ][0] + '\\dicts\\sakuin_homoph.json'
+        f = open(dict_path, 'r', encoding='utf-8')
+        global d_homoph
+        d_homoph = json.load(f)
+        f.close()
+        dict_homoph_created = True
+    # パッケージ化ための辞書読み込むなどまだ書いてないんです
+    # 入力の標準化 音韻情報だけ取り出す
+    if ' ' in word_phonetic or '　' in word_phonetic:
+        normalized_input = word_phonetic.split()[1]
+    else:
+        normalized_input = word_phonetic
+    res = d_homoph.get(normalized_input, [])
+    # 結果出力部分
+    if simple_output is not True:
+        return res
+    elif changeint is False:
+        return str(len(res))
+    else:
+        return len(res)
+
+
+def get_opcon(word_phonetic, simple_output=True):
+    # 高凝集で低結合のプログラミング（High Cohesion and Low Coupling）のために
+    # 音韻隣接語の判断匿名関数を作る
+    is_phonetic_friend = lambda l_target, l_jonb: 'Enemy' if len(
+        l_target) != len(l_jonb) else ('Friend' if sum([
+            1 if l_target[i] != l_jonb[i] else 0 for i in range(len(l_target))
+        ]) <= 1 else 'Enemy')
+    # 形態隣接語をゲット、そして表記と音韻情報を分割してdata_fileに保存
+    l_df = [word_phonetic.split()] + [
+        w.split()
+        for w in get_jonbs(word_phonetic.split()[0], simple_output=False)
+    ]
+    # 頻度を追加する
+    l_df = [w + [get_nttfreq(w[0], changeint=True)] for w in l_df]
+    # 音韻隣接語であるかどうかを判断
+    l_df = [l_df[0] + ['Target']] + [
+        w + [
+            is_phonetic_friend(get_mora(l_df[0][1], simple_output=False),
+                               get_mora(w[1], simple_output=False))
+        ] for w in l_df[1:]
+    ]
+    # 一貫性計算
+    l_jonb_friends = [w[2] for w in l_df if w[-1][0] == 'F']
+    l_jonb_all = [w[2] for w in l_df]
+    sum_target_friends = l_df[0][2] + sum(l_jonb_friends)
+    sum_all = sum(l_jonb_all)
+    if sum_target_friends != 0 and sum_all != 0:
+        op_onsistancy = sum_target_friends / sum_all
+    else:
+        op_onsistancy = 0
+    l_df[0].append(str(round(op_onsistancy, 3)))
+    l_df.append([])
+    if simple_output is True:
+        l_df = l_df[0][-1]
+    return l_df
+
+
+# headerとword_listこの２つの引数
+# word_listの中身は語彙＋発音　eg:大学＋だいがく
+# word_listはlistではなくでも可能が、　同じく語彙＋発音で
+# Defaultのheaderは全て可能のデータ
+def get_all(word_list, header=None, simple_output=True):
+    if header is None:
+        header = [
+            'Length', 'Mora', 'Nttfreq', 'Fam', 'Pfam', 'Jonbs', 'Jmnbs',
+            'Homoph', 'Opcon'
+        ]
+    # 一つの単語のデータを見たい場合でも対応可能
+    if type(word_list) != list:
+        word_list = [word_list]
+    # ヘッダーの整形、全部大文字にする
+    header = [s.title() for s in header]
+    operator = {
+        'Length': get_length,
+        'Mora': get_mora,
+        'Nttfreq': get_nttfreq,
+        'Fam': get_fam,
+        'Pfam': get_pfam,
+        'Jonbs': get_jonbs,
+        'Jmnbs': get_jmnbs,
+        'Homoph': get_homoph,
+        'Opcon': get_opcon
+    }
+    word_list = [[s] for s in word_list]
+    for ope in header:
+        for item in word_list:
+            item.append(operator.get(ope, lambda str: 'None')(item[0]))
+    # 出力をもっとキレイに見えるため、表記と発音を別々にバラす
+    word_list = [l[0].split() + l[1:] for l in word_list]
+    if simple_output is not True:
+        word_list.insert(0, ['Item', 'Pronunciation'] + header)
+    return word_list
